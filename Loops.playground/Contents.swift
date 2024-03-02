d407dd82d806c77de1aaa148db4639c63c595490
Ahmadal26 <132600897+Ahmadal26@users.noreply.github.com>
2024-03-02 05:22:04 +0300

    Done
    Done

---
+++ Loops.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+// Task 1: Iterating Over an Array
+let favoriteFruits = ["Apple", "Banana", "Cherry"]
+for fruit in favoriteFruits {
+    
+    print(fruit)
+    
+}
+
+// Task 2: Nested Loops with Strings
+for i in 1...5 {
+    for j in 1...5 {
+        let result = i * j
+     
+        print("\(i) * \(j) = \(result)")
+    }
+}
